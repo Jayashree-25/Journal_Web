bd1372cc54c2561ba69f0d0d24231fccb9140ae1
jayashre-25 <dasjayashree8260@gmail.com>
2025-06-19 11:18:08 +0530

    read and write function

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const port = 5000;
 
 app.use(cors());   // Enable frontend access
 app.use(bodyParser.json());   // Accept JSON input
+
+
+function readEntries(){
+    const data = fs.readFileSync("entries.json", "utf8");   //doubt
+    return JSON.parse(data);  //convert to JavaScript array 
+}
+
+function writeEntries(entries){
+    //entries: This is the JavaScript object or array you want to convert to a JSON string. 
+    //null: This means no special filtering (it includes all properties). 
+    //This specifies indentation of 2 spaces per level, making the output pretty-printed and easy to read.
+    fs.writeFileSyn("entries.json", JSON.stringify(entries,null,2));   //(value, replacer, space)
+}
