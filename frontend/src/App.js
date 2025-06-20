95a8cf062d6395067280a3a639d2f8739851c558
jayashre-25 <dasjayashree8260@gmail.com>
2025-06-20 19:49:29 +0530

    to show entries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//useEffect:  lets you run code when the component loads.. like if page loaded, do this..
+//useState: to store data
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  //entries: variable to hold journal data, setEntries: update the variable
+  const [entries, setEntries] = useState([]); //So at first, entries = []
+
+  useEffect(() => {
+    fetch("http://localhost:5000/entries")
+    .then(res => res.json())  //converts the response to json
+    .then(data => setEntries(data))  //saves the entries into our entries state
+    .catch(err => console.error("error in finding entries..!"));
+  }, []);  // Empty dependency array = run only once on page load
+
+  return(
+    <div style={{padding: "2rem", fontFamily: "Arial"}}>
+      <h1>My Journal</h1>
+
+      {entries.length === 0 ? (
+        <p>No journal entries yet</p>
+      ) : (
+        <ul>
+          {entries.map(entry => (  //loops through every entry
+            <li key={entry.id}>
+              <h3>{entry.title}</h3>
+              <p>{entry.content}</p>
+              <small>{new Date(entry.date).toLocaleString()}</small> 
+            </li>
+          ))}
+        </ul>
+      )}
+
     </div>
-  );
+  )
 }
-
 export default App;
