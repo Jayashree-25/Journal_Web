152c269e7fd05e4f97519419f1e40ce5bfe5ff80
jayashre-25 <dasjayashree8260@gmail.com>
2025-06-21 12:01:26 +0530

    edit function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,58 +2,97 @@
 //useState: to store data
 import React, { useEffect, useState } from "react";
 
-function App(){
+function App() {
   //entries: variable to hold journal data, setEntries: update the variable
   const [entries, setEntries] = useState([]); //So at first, entries = []
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const [editId, setEditId] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editContent, setEditContent] = useState("");
+
   useEffect(() => {
     fetch("http://localhost:5000/entries")
-    .then(res => res.json())  //converts the response to json
-    .then(data => setEntries(data))  //saves the entries into our entries state
-    .catch(err => console.error("error in finding entries..!"));
+      .then(res => res.json())  //converts the response to json
+      .then(data => setEntries(data))  //saves the entries into our entries state
+      .catch(err => console.error("error in finding entries..!"));
   }, []);  // Empty dependency array = run only once on page load
 
   //handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();  //prevent page reload
 
-    const newEntry = {title, content};
+    const newEntry = { title, content };
 
-    fetch("http://localhost:5000/entries" , {
-      method : "POST",
-      headers: {"Content-Type": "application/json"},
+    fetch("http://localhost:5000/entries", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newEntry),
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log("Added entry:", data);
-      setEntries(prevEntries => [data, ...prevEntries]);  //add new entry to top of list
-      setTitle("");  //clear form after submit
-      setContent("");
-    })
-    .catch(err => console.error("error in adding entries..!"));
+      .then(res => res.json())
+      .then(data => {
+        console.log("Added entry:", data);
+        setEntries(prevEntries => [data, ...prevEntries]);  //add new entry to top of list
+        setTitle("");  //clear form after submit
+        setContent("");
+      })
+      .catch(err => console.error("error in adding entries..!"));
   }
 
   //handle delete
   const handleDelete = (id) => {
-    fetch(`http://localhost:5000/entries/${id}` , {
+    fetch(`http://localhost:5000/entries/${id}`, {
       method: "DELETE",
     })
-    .then(() => {
-      //remove entry
-      setEntries(prevEntries => prevEntries.filter(entry => entry.id !== id));
-    })
-    .catch(err => console.error("error in deleting entries..!"));
+      .then(() => {
+        //remove entry
+        setEntries(prevEntries => prevEntries.filter(entry => entry.id !== id));
+      })
+      .catch(err => console.error("error in deleting entries..!"));
   }
 
-  return(
-    <div style={{padding: "2rem", fontFamily: "Arial"}}>
+  //when edit is clicked
+  const startEditing = (entry) => {
+    setEditId(entry.id);
+    setEditTitle(entry.title);
+    setEditContent(entry.content);
+  };
+  //cancel editing
+  const cancelEdit = () => {
+    setEditId(null);
+    setEditTitle("");
+    setEditContent("");
+  };
+
+  //update entry (PUT)
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    fetch(`http://localhost:5000/entries/${editId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: editTitle,
+        content: editContent,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedEntry) => {
+        console.log("server returned: ", updatedEntry)
+        setEntries(prevEntries => prevEntries.map(entry => (entry.id === editId ? updatedEntry : entry))
+        );
+        cancelEdit();
+      })
+      .catch((err) => console.error("cancelling error"));
+  }
+
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h1>My Journal</h1>
 
       <form onSubmit={handleSubmit} style={{ marginBottom: "2rem" }}>
-        
+
         <div style={{ marginBottom: "1rem" }}>
           <input
             type="text"
@@ -77,15 +116,15 @@ function App(){
         </div>
 
         <button
-        type="submit"
-        style={{
-          padding: "0.5rem 1rem",
-          backgroundColor: "#dc3545",
-          fontSize: "0.9rem",
-          color: "#fff",
-          border: "none",
-          borderRadius: "4px",
-        }}>
+          type="submit"
+          style={{
+            padding: "0.5rem 1rem",
+            backgroundColor: "#dc3545",
+            fontSize: "0.9rem",
+            color: "#fff",
+            border: "none",
+            borderRadius: "4px",
+          }}>
           Add Entry
         </button>
       </form>
@@ -95,33 +134,70 @@ function App(){
       ) : (
         <ul>
           {entries.map(entry => (  //loops through every entry
-            <li key={entry.id}>
-              <h3>{entry.title}</h3>
-              <p>{entry.content}</p>
-              <small>{new Date(entry.date).toLocaleString()}</small> 
+            <li key={entry.id} style={{ marginBottom: "1.5rem" }}>
+              {/* check if we are editing*/}
+              {editId === entry.id ? (
+                <form onSubmit={handleUpdate}>
+                  <input
+                    type="text"
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                    required
+                    style={{ width: "100%", padding: "0.5rem", marginBottom: "0.5rem" }}
+                  />
+                  <textarea
+                    value={editContent}
+                    onChange={(e) => setEditContent(e.target.value)}
+                    required
+                    style={{ width: "100%", padding: "0.5rem", marginBottom: "0.5rem" }}
+                  />
+                  <button type="submit" style={{ marginRight: "0.5rem" }}>Save</button>
+                  <button type="button" onClick={cancelEdit}>Cancel</button>
+                </form>
+              ) : (
+                <>
+                  <h3>{entry.title}</h3>
+                  <p>{entry.content}</p>
+                  <small>{new Date(entry.date).toLocaleString()}</small>
 
-              <br />
+                  <br />
 
-              <button
-                onClick={() => handleDelete(entry.id)}
-                style={{
-                  marginTop: "0.5rem",
-                  padding: "0.4rem 0.8rem",
-                  backgroundColor: "#dc3545",
-                  color: "white",
-                  border: "none",
-                  borderRadius: "4px",
-                  cursor: "pointer",
-                }}
-              >
-                Delete
-              </button>
+                  <button
+                    onClick={() => startEditing(entry)}
+                    style={{
+                      marginTop: "0.5rem",
+                      padding: "0.4rem 0.8rem",
+                      backgroundColor: "#17a2b8",
+                      color: "white",
+                      border: "none",
+                      borderRadius: "4px",
+                      cursor: "pointer",
+                      marginRight: "0.5rem"
+                    }}
+                  >
+                    Edit
+                  </button>
 
+                  <button
+                    onClick={() => handleDelete(entry.id)}
+                    style={{
+                      marginTop: "0.5rem",
+                      padding: "0.4rem 0.8rem",
+                      backgroundColor: "#dc3545",
+                      color: "white",
+                      border: "none",
+                      borderRadius: "4px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Delete
+                  </button>
+                </>
+              )}
             </li>
           ))}
         </ul>
       )}
-
     </div>
   )
 }
