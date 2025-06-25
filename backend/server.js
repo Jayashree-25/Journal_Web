226f48392b21757c647440d795560c352ceb4cc5
jayashre-25 <dasjayashree8260@gmail.com>
2025-06-25 20:51:22 +0530

    deleted

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,6 @@ app.use(cors());   // Enable frontend access
 app.use(bodyParser.json());   // Accept JSON input
 app.use(express.json());
 
-
-// function readEntries(){
-//     const data = fs.readFileSync("entries.json", "utf8");   //doubt
-//     return JSON.parse(data);  //convert to JavaScript array 
-// }
-
-// function writeEntries(entries){
-//     //entries: This is the JavaScript object or array you want to convert to a JSON string. 
-//     //null: This means no special filtering (it includes all properties). 
-//     //This specifies indentation of 2 spaces per level, making the output pretty-printed and easy to read.
-//     fs.writeFileSync("entries.json", JSON.stringify(entries,null,2));   //(value, replacer, space)
-// }
-
 //-------mongoDB connection-------//
 mongoose.connect("mongodb://localhost:27017/journalDB", {
     useNewUrlParser: true,
