6c535fe0795907d0b84a321467cc3260e781d528
jayashre-25 <dasjayashree8260@gmail.com>
2025-06-28 20:25:36 +0530

    cluster used

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());   // Accept JSON input
 app.use(express.json());
 
 //-------mongoDB connection-------//
-mongoose.connect("mongodb://localhost:27017/journalDB", {
+mongoose.connect("process.env.MONGO_URI", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
